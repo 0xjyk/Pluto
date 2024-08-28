7efe1b97534021a456be06503fd415a429243d7b
Jasraj Kripalani <63996321+0xjyk@users.noreply.github.com>
2024-08-28 06:37:49 -0700

    function and array parsing capabilities added

--- tt1.pp
+++
@@ -1,9 +0,0 @@
-# 1 "tests/parse-tests/tt1"
-# 1 "<built-in>"
-# 1 "<command-line>"
-# 31 "<command-line>"
-# 1 "/usr/include/stdc-predef.h" 1 3 4
-# 32 "<command-line>" 2
-# 1 "tests/parse-tests/tt1"
-# 25 "tests/parse-tests/tt1"
-int (*fun_arr[4])
